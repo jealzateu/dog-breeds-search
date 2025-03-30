12230dce93b9397ec22e03ade7c6761f3c699d7c
jealzateu <jealzateu@eafit.edu.co>
2025-03-30 10:03:09 -0500

    feat: add BreedSelectorComponent for selecting breeds and sub-breeds

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatButtonModule } from '@angular/material/button';
+import { BreedSelectorComponent } from './components/breed-selector/breed-selector.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BreedSelectorComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/breed-selector/breed-selector.component.ts
@@ -0,0 +1,50 @@
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { DogApiService } from '../../services/dog-api.service';
+
+@Component({
+  selector: 'app-breed-selector',
+  templateUrl: './breed-selector.component.html',
+  styleUrls: ['./breed-selector.component.css'],
+})
+export class BreedSelectorComponent implements OnInit {
+  breedsData: Record<string, string[]> = {};
+  breeds: string[] = [];
+  subBreeds: string[] = [];
+  selectedBreed: string = '';
+  selectedSubBreed: string = '';
+
+  @Output() breedSelected = new EventEmitter<{ breed: string; subBreed: string }>();
+
+  constructor(private readonly dogApiService: DogApiService) {}
+
+  ngOnInit(): void {
+    this.loadBreeds();
+  }
+
+  /**
+   * Loads all available breeds and their sub-breeds.
+   */
+  loadBreeds(): void {
+    this.dogApiService.getBreeds().subscribe((data) => {
+      this.breedsData = data.message;
+      this.breeds = Object.keys(this.breedsData);
+    });
+  }
+
+  /**
+   * Updates the list of sub-breeds based on the selected breed.
+   * @param breed The selected breed.
+   */
+  onBreedChange(breed: string): void {
+    this.selectedBreed = breed;
+    this.selectedSubBreed = '';
+    this.subBreeds = this.breedsData[breed] || [];
+  }
+
+  /**
+   * Emits the selected breed and sub-breed (if any).
+   */
+  onSearch(): void {
+    this.breedSelected.emit({ breed: this.selectedBreed, subBreed: this.selectedSubBreed });
+  }
+}
